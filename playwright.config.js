774c4081b246c348c99302ed55739280e047d1cc
Aetherinox <aetherinox@proton.me>
2024-07-24 07:42:06 -0700

    tests: enable parallel functionality

--- playwright.config.js
+++ playwright.config.js
@@ -15,7 +15,7 @@ const { defineConfig, devices } = require( '@playwright/test' )
 module.exports = defineConfig( {
     testDir: './tests',
     /* Run tests in files in parallel */
-    fullyParallel: false,
+    fullyParallel: true,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
